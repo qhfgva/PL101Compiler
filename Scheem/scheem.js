e560c2bcacba7abd41db438626e23c36265bca57
Dustin Lee <qhfgva@gmail.com>
2012-05-03 22:43:36 -0600

    parse and eval with tests

--- Scheem/scheem.js
+++ Scheem/scheem.js
@@ -1,20 +1,92 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('scheem.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+// console.log(parse("(a b c)"));
 
 var evalScheem = function (expr, env) {
     // Numbers evaluate to themselves
     if (typeof expr === 'number') {
         return expr;
     }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+    }
     // Look at head of list for operation
     switch (expr[0]) {
         case '+':
             return evalScheem(expr[1], env) +
                    evalScheem(expr[2], env);
+        case '-':
+            return evalScheem(expr[1], env) -
+                   evalScheem(expr[2], env);
+        case '*':
+            return evalScheem(expr[1], env) *
+                   evalScheem(expr[2], env);
+        case '/':
+            return evalScheem(expr[1], env) /
+                   evalScheem(expr[2], env);
         case 'quote':
             return expr[1];
+        case 'define':
+	    env[expr[1]] = evalScheem(expr[2],env);
+            return 0;
+        case 'set!':
+            env[expr[1]] = evalScheem(expr[2],env);
+            return 0;
+        case 'begin':
+            var result = 0;
+            for (var i=1; i<expr.length; i++) {
+                result = evalScheem(expr[i], env);
+            }
+            return result;
+        case '<':
+            var lt =
+                (evalScheem(expr[1], env) <
+                 evalScheem(expr[2], env));
+            if (lt) return '#t';
+            return '#f';
+        case 'cons':
+	    return [evalScheem(expr[1],env)].concat(evalScheem(expr[2],env));
+        case 'car':
+            return evalScheem(expr[1],env)[0];
+        case 'cdr':
+            var x = evalScheem(expr[1],env); 
+            return x.slice(1,x.length);
+        case '=':
+            var eq =
+                (evalScheem(expr[1], env) ===
+                 evalScheem(expr[2], env));
+            if (eq) return '#t';
+            return '#f';
+        case 'if':
+            var test = evalScheem(expr[1], env);
+            var result = '';
+            if (test == '#f') {
+                result = evalScheem(expr[3], env);
+            } 
+            else {
+                result = evalScheem(expr[2], env);
+            }
+            return result;
+
     }
 };
+
+var evalScheemString = function(scheemStr, env) {
+    return evalScheem(parse(scheemStr), env);
+};
+
 
 // If we are used as Node module, export evalScheem
 if (typeof module !== 'undefined') {
     module.exports.evalScheem = evalScheem;
+    module.exports.evalScheemString = evalScheemString;
 }
--- Scheem/test/evalScheemTest.js
+++ Scheem/test/evalScheemTest.js
@@ -2,6 +2,7 @@ if (typeof module !== 'undefined') {
     // In Node load required modules
     var assert = require('chai').assert;
     var evalScheem = require('../scheem').evalScheem;
+    var evalScheemString = require('../scheem').evalScheemString;
 } else {
     // In browser assume already loaded by <script> tags
     var assert = chai.assert;
@@ -24,6 +25,126 @@ suite('quote', function() {
         assert.deepEqual(
             evalScheem(['quote', [1, 2, 3]], {}),
             [1, 2, 3]
+        );
+    });
+});
+
+suite('numbers', function() {
+    test('a number', function() {
+        assert.deepEqual(
+            evalScheem(3, {}),
+            3
+        );
+    });
+});
+
+suite('variable references', function() {
+    test('a number', function() {
+        assert.deepEqual(
+  	    evalScheem('x', {x: 5}),
+            5
+        );
+    });
+    test('a math expression', function() {
+        assert.deepEqual(
+	    evalScheem(['/', 'z', ['+', 'x', 'y']], {x: 2, y: 3, z: 10}),
+            2
+        );
+    });
+});
+
+// suite('assignment', function() {
+//     test('define', function() {
+//         assert.deepEqual(
+//   	    evalScheem(['define' 'x' 5], {}),
+//             5
+//         );
+//     });
+//     test('set!', function() {
+//         assert.deepEqual(
+// 	    evalScheem(['/', 'z', ['+', 'x', 'y']], {x: 2, y: 3, z: 10}),
+//             2
+//         );
+//     });
+// });
+
+suite('begin', function() {
+    test('a number', function() {
+        assert.deepEqual(
+  	    evalScheem(['begin', ['set!', 'x', 5], 
+			         ['set!', 'x', ['+', 'y', 'x']], 
+			         'x'], {x:1, y:2}), 
+           7
+        );
+    });
+});
+
+suite('quoting', function() {
+    test('simple quote', function() {
+        assert.deepEqual(
+  	    evalScheem(['quote', ['+', 2, 3]], {}),
+            ['+', 2, 3]
+        );
+    });
+    test('nested quote', function() {
+        assert.deepEqual(
+  	    evalScheem(['quote', ['quote', ['+', 2, 3]]], {}),
+            ['quote', ['+', 2, 3]]
+        );
+    });
+});
+
+suite('numeric comparisons', function() {
+    test('less than - true', function() {
+        assert.deepEqual(
+  	    evalScheem(['<', ['+', 1, 1], ['+', 2, 3]], {}),
+            '#t'
+        );
+    });
+    test('less than - false', function() {
+        assert.deepEqual(
+  	    evalScheem(['<', 2, 2], {}), 
+            '#f'
+        );
+    });
+});
+
+suite('list manipulation', function() {
+    test('cons', function() {
+        assert.deepEqual(
+ 	   evalScheem(['cons', 1, ['quote', [3]]], {}), 
+           [1,3]//,3,5]
+        );
+    });
+    test('car', function() {
+        assert.deepEqual(
+  	   evalScheem(['car', ['quote',[1,2,3]]], {}), 
+           1
+        );
+    });
+    test('cdr', function() {
+        assert.deepEqual(
+	   evalScheem(['cdr', ['quote', [1,2,3]]], {}), 
+           [2,3]//,3,5]
+        );
+    });
+});
+
+suite('conditionals', function() {
+    test('if', function() {
+        assert.deepEqual(
+  	    evalScheem(['if', ['=', 'x', 5], 
+			1, 2], {x:5}), 
+           1
+        );
+    });
+});
+
+suite('end to end', function() {
+    test('simple parse and eval', function() {
+        assert.deepEqual(
+           evalScheemString("(+ 3 4)", {}), 
+           7
         );
     });
 });
